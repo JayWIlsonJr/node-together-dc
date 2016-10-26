1a71101d1afddf95e17319a8c67b494ee5b62423
Jay Wilson Jr <jwilson@ndgcommunications.com>
2016-10-25 20:21:20 -0400

    Add license

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(request, response) {
-	response.send('Yo!! You are the man!');
+	response.send('Yo!! You are the man!!');
 });
 
 module.exports = app;
